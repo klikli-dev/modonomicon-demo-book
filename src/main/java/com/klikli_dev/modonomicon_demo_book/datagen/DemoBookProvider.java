7a5e81bf38623b674c962295ca4ff863d19e3720
Kli Kli <klikli@outlook.at>
2022-09-26 18:21:53 +0200

    Add code to create book.json

--- src/main/java/com/klikli_dev/modonomicon_demo_book/datagen/DemoBookProvider.java
+++ src/main/java/com/klikli_dev/modonomicon_demo_book/datagen/DemoBookProvider.java
@@ -6,9 +6,12 @@
 
 package com.klikli_dev.modonomicon_demo_book.datagen;
 
+import com.klikli_dev.modonomicon.api.ModonomiconAPI;
+import com.klikli_dev.modonomicon.api.datagen.BookLangHelper;
 import com.klikli_dev.modonomicon.api.datagen.BookProvider;
 import com.klikli_dev.modonomicon.api.datagen.book.BookCategoryModel;
 import com.klikli_dev.modonomicon.api.datagen.book.BookEntryModel;
+import com.klikli_dev.modonomicon.api.datagen.book.BookEntryParentModel;
 import com.klikli_dev.modonomicon.api.datagen.book.BookModel;
 import com.mojang.logging.LogUtils;
 import net.minecraft.data.CachedOutput;
@@ -28,8 +31,32 @@ public class DemoBookProvider extends BookProvider {
         super(generator, modid);
     }
 
+    private BookModel makeDemoBook(String bookName) {
+        //The lang helper keeps track of the "DescriptionIds", that is, the language keys for translations, for us
+        var helper = ModonomiconAPI.get().getLangHelper(this.modid);
+
+        //we tell the helper the book we're in.
+        helper.book(bookName);
+
+        //Now we create the book with settings of our choosing
+        var demoBook = BookModel.builder()
+                .withId(this.modLoc(bookName)) //the id of the book. modLoc() prepends the mod id.
+                .withName(helper.bookName()) //the name of the book. The lang helper gives us the correct translation key.
+                .withTooltip(helper.bookTooltip()) //the hover tooltip for the book. Again we get a translation key.
+                .withGenerateBookItem(true) //auto-generate a book item for us.
+                .withModel(new ResourceLocation("modonomicon:modonomicon_red")) //use the default red modonomicon icon for the book
+                .withCreativeTab("modonomicon") //and put it in the modonomicon tab
+                .build();
+        return demoBook;
+    }
+
+
     @Override
     protected void generate() {
+        //call our code that generates a book with the id "demo"
+        var demoBook = this.makeDemoBook("demo");
 
+        //then add the book to the list of books to generate
+        this.add(demoBook);
     }
 }
