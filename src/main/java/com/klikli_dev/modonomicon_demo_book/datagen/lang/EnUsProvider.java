741e96b2f2e0b98e6a68542993c5f5f00111b9a7
Kli Kli <klikli@outlook.at>
2022-09-27 12:16:32 +0200

    Add text for the book

--- src/main/java/com/klikli_dev/modonomicon_demo_book/datagen/lang/EnUsProvider.java
+++ src/main/java/com/klikli_dev/modonomicon_demo_book/datagen/lang/EnUsProvider.java
@@ -6,6 +6,9 @@
 
 package com.klikli_dev.modonomicon_demo_book.datagen.lang;
 
+import com.klikli_dev.modonomicon.api.ModonomiconAPI;
+import com.klikli_dev.modonomicon.api.datagen.BookLangHelper;
+import com.klikli_dev.modonomicon_demo_book.ModonomiconDemoBook;
 import net.minecraft.data.DataGenerator;
 import net.minecraftforge.common.data.LanguageProvider;
 
@@ -15,7 +18,16 @@ public class EnUsProvider extends LanguageProvider {
         super(generator, modid, "en_us");
     }
 
-    protected void addTranslations() {
+    private void addDemoBook(){
+        //We again set up a lang helper to keep track of the translation keys for us.
+        //Forge language provider does not give us access to this.modid, so we get it from our main mod class
+        var helper = ModonomiconAPI.get().getLangHelper(ModonomiconDemoBook.MODID);
+        helper.book("demo"); //we tell the helper the book we're in.
+        this.add(helper.bookName(), "Demo Book"); //and now we add the actual textual book name
+        this.add(helper.bookTooltip(), "A book to showcase & test Modonomicon features."); //and the tooltip text
+    }
 
+    protected void addTranslations() {
+        this.addDemoBook();
     }
 }
