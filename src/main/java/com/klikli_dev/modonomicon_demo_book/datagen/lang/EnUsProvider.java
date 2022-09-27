b322dc68edae26e5f9c5c24b73f4e97bbb25ed95
Kli Kli <klikli@outlook.at>
2022-09-27 12:30:36 +0200

    Add text for entry and pages

--- src/main/java/com/klikli_dev/modonomicon_demo_book/datagen/lang/EnUsProvider.java
+++ src/main/java/com/klikli_dev/modonomicon_demo_book/datagen/lang/EnUsProvider.java
@@ -32,6 +32,24 @@ public class EnUsProvider extends LanguageProvider {
     private void addDemoBookFeaturesCategory(BookLangHelper helper) {
         helper.category("features"); //tell the helper the category we are in
         this.add(helper.categoryName(), "Features Category"); //annd provide the category name text
+
+        this.addDemoBookMultiblockEntry(helper);
+    }
+
+    private void addDemoBookMultiblockEntry(BookLangHelper helper) {
+        helper.entry("multiblock"); //tell the helper the entry we are in
+        this.add(helper.entryName(), "Multiblock Entry"); //provide the entry name
+        this.add(helper.entryDescription(), "An entry showcasing a multiblock."); //and description
+
+        helper.page("intro"); //now we configure the intro page
+        this.add(helper.pageTitle(), "Multiblock Page"); //page title
+        this.add(helper.pageText(), "Multiblock pages allow to preview multiblocks both in the book and in the world."); //page text
+
+        helper.page("multiblock"); //and finally the multiblock page
+        //now provide the multiblock name
+        //the lang helper does not handle multiblocks, so we manually add the same key we provided in the DemoBookProvider
+        this.add("multiblocks.modonomicon.blockentity", "Blockentity Multiblock.");
+        this.add(helper.pageText(), "A sample multiblock."); //and the multiblock page text
     }
 
     protected void addTranslations() {
