fc3beabcdb12afbb175e6cdcce40b32ed30f965b
Kli Kli <klikli@outlook.at>
2022-11-30 18:08:00 +0100

    Update to new datagen

--- src/main/java/com/klikli_dev/modonomicon_demo_book/datagen/DemoBookProvider.java
+++ src/main/java/com/klikli_dev/modonomicon_demo_book/datagen/DemoBookProvider.java
@@ -7,28 +7,16 @@
 package com.klikli_dev.modonomicon_demo_book.datagen;
 
 import com.klikli_dev.modonomicon.api.ModonomiconAPI;
-import com.klikli_dev.modonomicon.api.datagen.BookLangHelper;
 import com.klikli_dev.modonomicon.api.datagen.BookProvider;
-import com.klikli_dev.modonomicon.api.datagen.book.BookCategoryModel;
-import com.klikli_dev.modonomicon.api.datagen.book.BookEntryModel;
-import com.klikli_dev.modonomicon.api.datagen.book.BookEntryParentModel;
 import com.klikli_dev.modonomicon.api.datagen.book.BookModel;
-import com.mojang.logging.LogUtils;
-import net.minecraft.data.CachedOutput;
 import net.minecraft.data.DataGenerator;
-import net.minecraft.data.DataProvider;
 import net.minecraft.resources.ResourceLocation;
-import org.slf4j.Logger;
-
-import java.io.IOException;
-import java.nio.file.Path;
-import java.util.HashMap;
-import java.util.Map;
+import net.minecraftforge.common.data.LanguageProvider;
 
 public class DemoBookProvider extends BookProvider {
 
-    public DemoBookProvider(DataGenerator generator, String modid) {
-        super(generator, modid);
+    public DemoBookProvider(DataGenerator generator, String modid, LanguageProvider lang) {
+        super(generator, modid, lang);
     }
 
     private BookModel makeDemoBook(String bookName) {
