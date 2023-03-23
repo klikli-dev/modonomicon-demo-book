8166481511ed6b791a5cce52a537dcb29260e55e
Kli Kli <klikli@outlook.at>
2023-03-23 18:52:49 +0100

    chore: update book gen to fit new modonomicon version

--- src/main/java/com/klikli_dev/modonomicon_demo_book/datagen/DemoBookProvider.java
+++ src/main/java/com/klikli_dev/modonomicon_demo_book/datagen/DemoBookProvider.java
@@ -27,16 +27,17 @@ public class DemoBookProvider extends BookProvider {
         helper.book(bookName);
 
         //Now we create the book with settings of our choosing
-        var demoBook = BookModel.builder()
-                .withId(this.modLoc(bookName)) //the id of the book. modLoc() prepends the mod id.
-                .withName(helper.bookName()) //the name of the book. The lang helper gives us the correct translation key.
+        var demoBook = BookModel.create(
+                    this.modLoc(bookName), //the id of the book. modLoc() prepends the mod id.
+                    helper.bookName() //the name of the book. The lang helper gives us the correct translation key.
+                )
                 .withTooltip(helper.bookTooltip()) //the hover tooltip for the book. Again we get a translation key.
                 .withGenerateBookItem(true) //auto-generate a book item for us.
                 .withModel(new ResourceLocation("modonomicon:modonomicon_red")) //use the default red modonomicon icon for the book
                 .withCreativeTab("modonomicon") //and put it in the modonomicon tab
                 //Important: On 1.19.3 the creative tab takes a resource location:
                 //           .withCreativeTab(new ResourceLocation("modonomicon","modonomicon"))
-                .build();
+                ;
         return demoBook;
     }
 
