394697e71854b61dad627c04a1239976df24581c
Kli Kli <klikli@outlook.at>
2023-06-15 19:17:01 +0200

    feat: update to latest modo lang features

--- src/main/java/com/klikli_dev/modonomicon_demo_book/datagen/DemoBookProvider.java
+++ src/main/java/com/klikli_dev/modonomicon_demo_book/datagen/DemoBookProvider.java
@@ -6,10 +6,8 @@
 
 package com.klikli_dev.modonomicon_demo_book.datagen;
 
-import com.klikli_dev.modonomicon.api.ModonomiconAPI;
 import com.klikli_dev.modonomicon.api.datagen.BookProvider;
 import com.klikli_dev.modonomicon.api.datagen.book.BookModel;
-import net.minecraft.data.DataGenerator;
 import net.minecraft.data.PackOutput;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.common.data.LanguageProvider;
@@ -17,36 +15,26 @@ import net.minecraftforge.common.data.LanguageProvider;
 public class DemoBookProvider extends BookProvider {
 
     public DemoBookProvider(PackOutput packOutput, String modid, LanguageProvider lang) {
-        super(packOutput, modid, lang);
+        super("demo", packOutput, modid, lang);
     }
 
-    private BookModel makeDemoBook(String bookName) {
-        //The lang helper keeps track of the "DescriptionIds", that is, the language keys for translations, for us
-        var helper = ModonomiconAPI.get().getLangHelper(this.modid);
-
-        //we tell the helper the book we're in.
-        helper.book(bookName);
-
-        //Now we create the book with settings of our choosing
+    @Override
+    protected BookModel generateBook() {
         var demoBook = BookModel.create(
-                    this.modLoc(bookName), //the id of the book. modLoc() prepends the mod id.
-                    helper.bookName() //the name of the book. The lang helper gives us the correct translation key.
+                        this.modLoc(this.context().book), //the id of the book. modLoc() prepends the mod id.
+                        this.context().bookName() //the name of the book. The lang helper gives us the correct translation key.
                 )
-                .withTooltip(helper.bookTooltip()) //the hover tooltip for the book. Again we get a translation key.
+                .withTooltip(this.context().bookTooltip()) //the hover tooltip for the book. Again we get a translation key.
                 .withGenerateBookItem(true) //auto-generate a book item for us.
                 .withModel(new ResourceLocation("modonomicon:modonomicon_red")) //use the default red modonomicon icon for the book
                 .withCreativeTab(new ResourceLocation("modonomicon", "modonomicon")) //and put it in the modonomicon tab
                 ;
+
         return demoBook;
     }
 
-
     @Override
-    protected void generate() {
-        //call our code that generates a book with the id "demo"
-        var demoBook = this.makeDemoBook("demo");
-
-        //then add the book to the list of books to generate
-        this.add(demoBook);
+    protected void registerDefaultMacros() {
+        //currently we have no macros - they only work with the CategoryProviders
     }
 }
