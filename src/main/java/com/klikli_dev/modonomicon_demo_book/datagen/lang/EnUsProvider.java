ad02173976431d95dce7f9bf8d4bb330e34e5d26
Kli Kli <klikli@outlook.at>
2023-06-15 11:26:10 +0200

    fix: update related issues

--- src/main/java/com/klikli_dev/modonomicon_demo_book/datagen/lang/EnUsProvider.java
+++ src/main/java/com/klikli_dev/modonomicon_demo_book/datagen/lang/EnUsProvider.java
@@ -10,12 +10,13 @@ import com.klikli_dev.modonomicon.api.ModonomiconAPI;
 import com.klikli_dev.modonomicon.api.datagen.BookLangHelper;
 import com.klikli_dev.modonomicon_demo_book.ModonomiconDemoBook;
 import net.minecraft.data.DataGenerator;
+import net.minecraft.data.PackOutput;
 import net.minecraftforge.common.data.LanguageProvider;
 
 public class EnUsProvider extends LanguageProvider {
 
-    public EnUsProvider(DataGenerator generator, String modid) {
-        super(generator, modid, "en_us");
+    public EnUsProvider(PackOutput packOutput, String modid) {
+        super(packOutput, modid, "en_us");
     }
 
     private void addDemoBook(){
