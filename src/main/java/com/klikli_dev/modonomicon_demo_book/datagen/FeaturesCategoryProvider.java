c137a26f7c66bb0460978f5c43371fc28689432f
Kli Kli <klikli@outlook.at>
2023-07-03 08:23:32 +0200

    fix: merge issues

--- src/main/java/com/klikli_dev/modonomicon_demo_book/datagen/FeaturesCategoryProvider.java
+++ src/main/java/com/klikli_dev/modonomicon_demo_book/datagen/FeaturesCategoryProvider.java
@@ -34,17 +34,21 @@ public class FeaturesCategoryProvider extends CategoryProvider {
 
     @Override
     protected BookCategoryModel generateCategory() {
+        this.lang().add(this.context().categoryName(), "Features Category"); //provide the category name text
+
         return BookCategoryModel.create(
                         this.modLoc(this.context().categoryId()), //the id of the category, as stored in the lang helper. modLoc() prepends the mod id.
                         this.context().categoryName() //the name of the category. The lang helper gives us the correct translation key.
                 )
                 .withIcon("minecraft:nether_star"); //the icon for the category. In this case we simply use an existing item.
-
     }
 
 
     private BookEntryModel makeMultiblockEntry(char location) {
         this.context().entry("multiblock"); //tell our context the entry we are in. It puts it on top of the category.
+        this.lang().add(this.context().entryName(), "Multiblock Entry"); //provide the entry name
+        this.lang().add(this.context().entryDescription(), "An entry showcasing a multiblock."); //and description
+
 
         this.context().page("intro"); //and now the page
         var multiBlockIntroPage =
@@ -52,6 +56,8 @@ public class FeaturesCategoryProvider extends CategoryProvider {
                         .withText(this.context().pageText()) //now we can use the context to retrieve the description id for the text
                         .withTitle(this.context().pageTitle()) //and for the title
                         .build();
+        this.lang().add(this.context().pageTitle(), "Multiblock Page"); //page title
+        this.lang().add(this.context().pageText(), "Multiblock pages allow to preview multiblocks both in the book and in the world."); //page text
 
         this.context().page("multiblock"); //next page
         var multiblockPreviewPage =
@@ -60,6 +66,11 @@ public class FeaturesCategoryProvider extends CategoryProvider {
                         .withMultiblockName("multiblocks.modonomicon_demo_book.blockentity") //and the lang key for its name
                         .withText(this.context().pageText()) //plus a page text
                         .build();
+        //now provide the multiblock name
+        //the lang helper does not handle multiblocks, so we manually add the same key we provided in the DemoBookProvider
+        this.lang().add("multiblocks.modonomicon_demo_book.blockentity", "Blockentity Multiblock.");
+        //and the multiblock page text
+        this.lang().add(this.context().pageText(), "A sample multiblock.");
 
         return this.entry(location) //create a new entry at the given location
                 .withIcon("minecraft:furnace") //we use furnace as icon
